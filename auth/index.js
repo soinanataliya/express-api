a0eb2931085835c6c49f3ede6677586cbe5ff9b5
Nataliya Soina <natashaford@mail.ru>
2024-09-08 18:57:42 +0300

    add postgress database

--- auth/index.js
+++ auth/index.js
@@ -4,6 +4,22 @@ const nunjucks = require("nunjucks");
 const { nanoid } = require("nanoid");
 const crypto = require("crypto");
 const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const knex = require("knex");
+const knexConfig = require("./knexfile");
+
+const dbConnection = knex(knexConfig);
+
+// const checkKnex = () => {
+//   dbConnection.schema.hasTable("timers").then(
+//     function (exists) {
+//       console.log("exists", exists);
+//     },
+//     function (err) {
+//       console.log("[db] Could not check subroutine table: " + err.stack);
+//     }
+//   );
+// };
 
 const app = express();
 
@@ -28,44 +44,12 @@ app.use(cookieParser());
 
 const hash = (str) => crypto.createHash("sha256").update(str).digest("hex");
 
-let TIMERS = [
-  {
-    start: Date.now(),
-    description: "Timer 1",
-    isActive: true,
-    id: nanoid(),
-    userId: "1",
-  },
-  {
-    start: Date.now() - 5000,
-    end: Date.now() - 3000,
-    duration: 2000,
-    description: "Timer 0",
-    isActive: false,
-    id: nanoid(),
-    userId: "1",
-  },
-];
-
-let DB = {
-  users: [
-    {
-      _id: "1",
-      username: "admin",
-      password: hash("admin"),
-    },
-  ],
-  sessions: {},
-  timers: TIMERS,
-};
-
 const getNewTimer = (description, user) => {
   return {
     start: Date.now(),
     description,
     isActive: true,
-    id: nanoid(),
-    userId: user._id,
+    userId: user.id,
   };
 };
 
@@ -74,8 +58,10 @@ const auth = () => async (req, res, next) => {
     return next();
   }
   const user = await findUserBySessionId(req.cookies["sessionId"]);
+
   req.user = user;
   req.sessionId = req.cookies["sessionId"];
+
   next();
 };
 
@@ -89,9 +75,11 @@ app.get("/", auth(), (req, res) => {
 app.get("/api/timers", auth(), async (req, res) => {
   try {
     const user = await findUserBySessionId(req.cookies["sessionId"]);
-
     const { isActive } = req.query;
-    const result = TIMERS.filter((timer) => timer.userId === user._id)
+
+    const userTimers = await dbConnection("timers").select().where("userId", user.id);
+
+    const result = userTimers
       .map((timer) => {
         if (timer.isActive && isActive === "true") {
           return {
@@ -109,7 +97,8 @@ app.get("/api/timers", auth(), async (req, res) => {
       })
       .filter((timer) => !!timer);
     res.json(result);
-  } catch {
+  } catch (error) {
+    console.log(error);
     res.sendStatus(500);
   }
 });
@@ -121,21 +110,35 @@ app.post("/api/timers", auth(), async (req, res) => {
     const { description } = req.body;
 
     const timer = getNewTimer(description, user);
-    TIMERS.push(timer);
 
-    res.status(201).json({ description, id: timer.id });
-  } catch {
+    const idsArray = await dbConnection("timers").insert(timer).returning("id");
+
+    res.status(201).json({ description, id: idsArray[0].id });
+  } catch (error) {
+    console.log("error ====>", error);
     res.sendStatus(400);
   }
 });
 
-app.post("/api/timers/:id/stop", auth(), (req, res) => {
+app.post("/api/timers/:id/stop", auth(), async (req, res) => {
   try {
     const id = req.params.id;
-    const deletedTimer = TIMERS.find((timer) => timer.id === id);
-    deletedTimer.end = Date.now();
-    deletedTimer.isActive = false;
-    deletedTimer.duration = deletedTimer.end - deletedTimer.start;
+
+    const deletedTimers = await dbConnection("timers").select().where("id", id);
+
+    if (!deletedTimers.length) {
+      return res.sendStatus(404);
+    }
+    const deletedTimer = deletedTimers[0];
+    const end = Date.now();
+    const newTimerData = {
+      end,
+      isActive: false,
+      duration: end - deletedTimer.start,
+    };
+
+    await dbConnection("timers").update(newTimerData).where("id", deletedTimer.id);
+
     res.status(201).json({ id });
   } catch {
     res.sendStatus(500);
@@ -144,33 +147,53 @@ app.post("/api/timers/:id/stop", auth(), (req, res) => {
 
 // auth
 
-const createSession = async (userId) => {
+const createSession = async (userName) => {
   const sessionId = nanoid();
-  DB.sessions[sessionId] = userId;
+  await dbConnection("sessions").insert({
+    sessionId,
+    username: userName,
+  });
   return sessionId;
 };
+
 const deleteSession = async (sessionId) => {
-  delete DB.sessions[sessionId];
+  return dbConnection("sessions").where("sessionId", sessionId).delete();
 };
 
 const findUserBySessionId = async (sessionId) => {
-  const userId = DB.sessions[sessionId];
-  if (!userId) {
-    return;
+  const usernames = await dbConnection("sessions").select("username").where("sessionId", sessionId).limit(1);
+
+  if (!usernames.length) {
+    return null;
   }
-  return DB.users.find((user) => user._id === userId);
+  const users = await dbConnection("users").select().where("username", usernames[0].username);
+
+  if (!users.length) {
+    return null;
+  }
+
+  return users[0];
 };
 
-const findUserByUsername = async (username) => DB.users.find((user) => user.username === username);
+const findUserByUsername = async (username) => {
+  const users = await dbConnection("users").select().where("username", username);
+  if (!users.length) {
+    return;
+  }
+  return users[0];
+};
 
 app.post("/login", bodyParser.urlencoded({ extended: false }), async (req, res) => {
   try {
     const { username, password } = req.body;
+
     const user = await findUserByUsername(username);
+
     if (!user || user?.password !== hash(password)) {
       return res.redirect("/?authError=true");
     }
-    const sessionId = await createSession(user._id);
+    const sessionId = await createSession(user.username);
+
     res.cookie("sessionId", sessionId, { httpOnly: true }).redirect("/");
   } catch {
     res.status(400);
@@ -189,16 +212,17 @@ app.post("/signup", bodyParser.urlencoded({ extended: false }), async (req, res)
   try {
     const { username, password } = req.body;
     const user = await findUserByUsername(username);
+
     if (user) {
       return res.status(400).json({ msg: "User exists" });
     }
-    const id = nanoid();
-    DB.users.push({
-      _id: id,
+
+    await dbConnection("users").insert({
       username,
       password: hash(password),
     });
-    const sessionId = await createSession(id);
+
+    const sessionId = await createSession(username);
     res.cookie("sessionId", sessionId, { httpOnly: true }).redirect("/");
   } catch {
     res.status(400);
